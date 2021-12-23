e509e5c2e769daace25494a4239d1c3432808b37
HaseebAliKhan <57507467+HaseebAliKhan@users.noreply.github.com>
2021-12-23 19:04:42 +0500

    update

--- ChatBot/chatbot.js
+++ ChatBot/chatbot.js
@@ -1,6 +1,6 @@
 'use strict'
 const dialogflow = require('dialogflow');
-const config = require('../config/keys');
+const config = require('../config/prod');
 const structjson = require('structjson')
 
 const projectID = config.googleProjectID;
