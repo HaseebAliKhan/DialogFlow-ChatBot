455dccb7cde97b7a8b5ab0cb459b3c11c7aa5956
HaseebAliKhan <57507467+HaseebAliKhan@users.noreply.github.com>
2021-12-23 18:47:46 +0500

    Update chatbot.js

--- ChatBot/chatbot.js
+++ ChatBot/chatbot.js
@@ -18,55 +18,55 @@ const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dia
 
 module.exports = {
     textQuery: async function(text, parameters = {}){
+      let self = module.exports;
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: text,
+            // The language used by the client (en-US)
+            languageCode: config.dialogFlowSessionLanguageCode,
+          },
+        },
+        queryParams: {
+          payload: {
+            data: parameters
+          }
+        }
+      };
       try {
-        let self = module.exports;
-        const request = {
-            session: sessionPath,
-            queryInput: {
-              text: {
-                // The query to send to the dialogflow agent
-                text: text,
-                // The language used by the client (en-US)
-                languageCode: config.dialogFlowSessionLanguageCode,
-              },
-            },
-            queryParams: {
-                payload: {
-                    data: parameters
-                }
-            }
-          };
             let responses = await sessionClient.detectIntent(request);
           responses = await self.handleAction(responses);
           return responses;  
           } catch (error) {
-            console.error(error);
+            console.log(error);
           }
           // Send request and log result
           
     },
     eventQuery: async function(event, parameters = {}){
+      let self = module.exports;
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          event: {
+            // The query to send to the dialogflow agent
+            name: event,
+            parameters:structjson.structProtoToJson(parameters),
+            // The language used by the client (en-US)
+            languageCode: config.dialogFlowSessionLanguageCode,
+          },
+        },
+      };
       try {
-        let self = module.exports;
-        const request = {
-            session: sessionPath,
-            queryInput: {
-              event: {
-                // The query to send to the dialogflow agent
-                name: event,
-                parameters:structjson.structProtoToJson(parameters),
-                // The language used by the client (en-US)
-                languageCode: config.dialogFlowSessionLanguageCode,
-              },
-            },
-          };
          
           // Send request and log result
             let responses = await sessionClient.detectIntent(request);
           responses = await self.handleAction(responses);
           return responses;  
           } catch (error) {
-            console.error(error);
+            console.log(error);
           }
           
     },
@@ -76,7 +76,7 @@ module.exports = {
 
         return responses
       } catch (error) {
-            console.error(error);
+            console.log(error);
           }
     }
 } 
