2f2a02fd6747ccfb91de5ceb07c9efd70ba19927
HaseebAliKhan <57507467+HaseebAliKhan@users.noreply.github.com>
2021-12-23 18:44:41 +0500

    up

--- routes/DialogFlowRoutes.js
+++ routes/DialogFlowRoutes.js
@@ -6,12 +6,23 @@ module.exports = app => {
         res.send({"hello":"There"})
     });
     app.post('/api/df-text-query',async(req,res)=>{
-        let responses = await chatbot.textQuery(req.body.text, req.body.parameters)
-        
-        res.send(responses[0].queryResult)
+        try {
+            let responses = await chatbot.textQuery(req.body.text, req.body.parameters)
+            
+            res.send(responses[0].queryResult)
+            
+        } catch (error) {
+            console.log(error)
+        }
+            
     });
     app.post('/api/df-event-query', async(req,res)=>{
-      let responses = await chatbot.eventQuery(req.body.event, req.body.parameters)
-      res.send(responses[0].queryResult)
+        try {
+            
+            let responses = await chatbot.eventQuery(req.body.event, req.body.parameters)
+            res.send(responses[0].queryResult)
+        } catch (error) {
+            console.log(error)
+        }
     });
 }
